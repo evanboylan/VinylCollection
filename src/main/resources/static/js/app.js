88cd866ec95b8b8783d534055b966219e9f9ee96
evanboylan <eboylan09@gmail.com>
2022-02-17 20:00:29 -0500

    finished backend work, completed app.js, finished artists view

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,6 @@
 import {displayAllRecordsView} from "./records.js";
+import {displayAllArtistsView} from "./artists.js";
+import {displayAllCategoriesView} from "./categories.js";
 import {displayHomeView} from "./home.js";
 
 const containerEl = document.querySelector(".container");
@@ -14,15 +16,84 @@ function clearChildren(element) {
 };
 
 function buildHeader() {
+    const sectionEl = document.createElement("section");
+    const headerEl = document.createElement("header");
+    headerEl.id = "header";
+
+    const headerLogoEl = document.createElement("img");
+    headerLogoEl.src = "images/logo.jpg";
+    headerLogoEl.alt = "site logo";
+    headerLogoEl.classlist.add("logo");
+    headerLogoEl.classList.add("clickable_el");
+    headerLogoEl.addEventListener("click", () => {
+        window.location = "./index.html";
+
+    })
+
+    const pageLinksEl = document.createElement("div");
+    pageLinksEl.classList.add("page_links");
+
+    const allRecordsEl = document.createElement("p");
+    allRecordsEl.innerText = "Records";
+    allRecordsEl.classList.add("nav_link");
+    allRecordsEl.classList.add("clickable_el");
+    allRecordsEl.addEventListener("click", () => {
+        const mainEl = document.querySelector(".main-content");
+        clearChildren(mainEl);
+        displayAllRecordsView(mainEl);
+    })
+
+    const allCategoriesEl = document.createElement("p");
+    allCategoriesEl.innerText = "Categories";
+    allCategoriesEl.classList.add("nav_link");
+    allCategoriesEl.classList.add("clickable_el");
+    allCategoriesEl.addEventListener("click", () => {
+        const mainEl = document.querySelector(".main-content");
+        clearChildren(mainEl);
+        displayAllCategoriesView(mainEl);
+    })
+
+    const allArtistsEl = document.createElement("p");
+    allArtistsEl.innerText = "Artists";
+    allArtistsEl.classList.add("nav_link");
+    allArtistsEl.classList.add("clickable_el");
+    allArtistsEl.addEventListener("click", () => {
+        const mainEl = document.querySelector(".main-content");
+        clearChildren(mainEl);
+        displayAllArtistsView(mainEl);
+    })
+
+    pageLinksEl.appendChild(allRecordsEl);
+    pageLinksEl.appendChild(allCategoriesEl);
+    pageLinksEl.appendChild(allArtistsEl);
+
+    headerEl.appendChild(headerLogoEl);
+    headerEl.appendChild(pageLinksEl);
+
+    sectionEl.appendChild(headerEl);
+
+    containerEl.appendChild(headerEl);
 
 }
 
 function buildMainElement(){
 
+    const mainEl = document.createElement("main");
+    mainEl.classList.add("main-content");
+    containerEl.appendChild(mainEl);
+    clearChildren(mainEl);
+    displayHomeView(mainEl);
 }
 
 function buildFooter(){
-
+    const footerEl = document.createElement("footer");
+    const footerDivEl = document.createElement("div");
+    footerDivEl.classList.add("footer_text");
+    const footerTextEl = document.createElement("p");
+    footerTextEl.innerText = "Evan Boylan - 2022";
+    footerDivEl.appendChild(footerTextEl);
+    footerEl.appendChild(footerDivEl);
+    containerEl.appendChild(footerEl);
 }
 
 export {
